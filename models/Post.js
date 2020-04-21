d78c84cd5dc21ac4f22c79846045d5dd143391a3
Igor Zaika <izaika89@gmail.com>
2020-04-21 22:07:37 +0300

    mongoose schemas

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const { model, Schema } = require('mongoose');
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: [String],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  messages: [
+    {
+      messageBody: {
+        type: String,
+        required: true,
+      },
+      messageDate: {
+        type: Date,
+        default: Date.now,
+      },
+      messageUser: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
+    },
+  ],
+});
+
+module.exports = model('Post', PostSchema);
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
 
 mongoose
   .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,7 +28,7 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, context: { User, Post } });
 
 server.listen().then(({ url }) => {
   console.log(`Server is listening on port ${url}`);
