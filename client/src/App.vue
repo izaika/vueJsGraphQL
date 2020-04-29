01becf47289bc8a55c79574dbf0b4f90f4fe59c5
Igor Zaika <izaika89@gmail.com>
2020-04-29 09:45:34 +0300

    side nav bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <v-app>
+  <v-app style="background: #e3e3ee">
+    <v-navigation-drawer app temporary fixed v-model="sideNav">
+      <v-toolbar color="accent" dark flat>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+        <router-link to="/" tag="h1" class="title pl-3" style="cursor: pointer">VueShare</router-link>
+      </v-toolbar>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-card dark color="primary" flat tile>
       <v-toolbar>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
         <v-toolbar-title>
-          <router-link to="/" tag="span" style="cursor: pointer"
-            >VueShare</router-link
-          >
+          <router-link to="/" tag="span" style="cursor: pointer">VueShare</router-link>
         </v-toolbar-title>
 
         <v-spacer></v-spacer>
@@ -23,13 +41,9 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items>
-          <v-btn
-            v-for="item in horizontalNavItems"
-            :key="item.title"
-            :to="item.link"
-          >
-            <v-icon>{{ item.icon }}</v-icon
-            >{{ item.title }}
+          <v-btn v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+            <v-icon>{{ item.icon }}</v-icon>
+            {{ item.title }}
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
@@ -44,13 +58,27 @@
 <script>
 export default {
   name: 'App',
+
+  data: () => ({
+    sideNav: false,
+  }),
+
   computed: {
-    horizontalNavItems() {
-      return [
-        { icon: 'mdi-message-text', title: 'Posts', link: '/posts' },
-        { icon: 'mdi-lock-open-outline', title: 'Sign In', link: '/signin' },
-        { icon: 'mdi-pencil', title: 'Sign Up', link: '/signup' },
-      ];
+    horizontalNavItems: () => [
+      { icon: 'mdi-message-text', title: 'Posts', link: '/posts' },
+      { icon: 'mdi-lock-open-outline', title: 'Sign In', link: '/signin' },
+      { icon: 'mdi-pencil', title: 'Sign Up', link: '/signup' },
+    ],
+    sideNavItems: () => [
+      { icon: 'mdi-message-text', title: 'Posts', link: '/posts' },
+      { icon: 'mdi-lock-open-outline', title: 'Sign In', link: '/signin' },
+      { icon: 'mdi-pencil', title: 'Sign Up', link: '/signup' },
+    ],
+  },
+
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     },
   },
 };
