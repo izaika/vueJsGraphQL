cde3452d1031e132fe8e20924f44850467ded214
Igor Zaika <izaika89@gmail.com>
2020-05-01 17:20:03 +0300

    apollo client setup

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
+
+Vue.use(VueApollo);
+
+export const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const apolloProvider = new VueApollo({ defaultClient: apolloClient });
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { gql } from 'apollo-boost';
 
-Vue.use(Vuex)
+import { apolloClient } from '../main';
+import { GET_POSTS } from '../queries';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    posts: [],
+    loaders: [],
   },
   mutations: {
+    setPosts: (state, payload) => (state.posts = payload),
+    addLoader: (state, payload) => {
+      state.loaders.push(payload);
+    },
+    removeLoader: (state, payload) => {
+      state.loaders = state.loaders.filter(loader => loader !== payload);
+    },
   },
   actions: {
+    getPosts: async ({ commit }) => {
+      commit('addLoader', 'getPosts');
+
+      const {
+        data: { getPosts },
+      } = await apolloClient.query({ query: GET_POSTS });
+
+      commit('removeLoader', 'getPosts');
+      commit('setPosts', getPosts);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    posts: ({ posts }) => posts,
+    isLoading: ({ loaders }) => !!loaders.length,
+  },
+});
