e99472c908a8765a5e1d000f59ceb0a663399104
Igor Zaika <izaika89@gmail.com>
2020-05-02 12:43:13 +0300

    Create Gravatar Avatar and Hash User Passwords on Signup

--- models/User.js
+++ models/User.js
@@ -1,35 +1,48 @@
 import { model, Schema } from 'mongoose';
+import md5 from 'md5';
+import bcrypt from 'bcrypt';
 
-export const User = model(
-  'User',
-  new Schema({
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    avatar: {
-      type: String,
-    },
-    joinDate: {
-      type: Date,
-      default: Date.now,
-    },
-    favorites: {
-      type: [Schema.Types.ObjectId],
-      required: true,
-      ref: 'Post',
-    },
-  })
-);
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  avatar: {
+    type: String,
+  },
+  joinDate: {
+    type: Date,
+    default: Date.now,
+  },
+  favorites: {
+    type: [Schema.Types.ObjectId],
+    required: true,
+    ref: 'Post',
+  },
+});
+
+UserSchema.pre('save', function (next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+export const User = model('User', UserSchema);
